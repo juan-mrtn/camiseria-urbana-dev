a96ee9c1e6f9bb593492b6f09913b5d6e1ed163c
juan-mrtn <juan.acuna.jma@gmail.com>
2026-01-08 17:50:42 -0300

    prisma dependencias

---
+++ src/lib/db.ts
@@ -0,0 +1,13 @@
+import { PrismaClient } from "@prisma/client/extension";
+
+const prismaClientSingleton = () => {
+  return new PrismaClient();
+};
+
+declare global {
+  var prisma: undefined | ReturnType<typeof prismaClientSingleton>;
+}
+
+export const db = globalThis.prisma ?? prismaClientSingleton();
+
+if (process.env.NODE_ENV !== "production") globalThis.prisma = db;
